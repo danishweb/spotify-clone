2b4a51f32fc80b11262b7f14a01ca9fb4bc3516f
DanishShaikh-AsIndia <Danish.Shaikh@asindia.net>
2022-02-14 21:12:03 +0530

    Final Build

---
+++ components/Songs.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { playlistState } from "./../atoms/playlistAtom";
+import Song from "./Song";
+
+function Songs() {
+  const playlists = useRecoilValue(playlistState);
+  return (
+    <div className="px-8 flex flex-col space-y-1 pb-28 text-white">
+      {playlists?.tracks.items.map((track, i) => (
+        <Song key={track.track.id} track={track} order={i} />
+      ))}
+    </div>
+  );
+}
+
+export default Songs;
